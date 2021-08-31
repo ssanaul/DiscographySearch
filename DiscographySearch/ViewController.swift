bd2580d70762b008193dd56a1cfab1e1abd1f987
Sulaiman Sanaullah <ss7@illinois.edu>
2021-08-31 11:06:12 -0500

    Initial Commit

---
+++ DiscographySearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiscographySearch
+//
+//  Created by Sul S. on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
